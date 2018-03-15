aa0378fc3cd6b011dc1f35d0e10dfe79c466086a
1174062992 <1174062992>
2018-03-15 17:00:03 +0800

    Initial Commit

---
+++ iPhone指纹认证/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhone指纹认证
+//
+//  Created by 曹记 on 2018/3/15.
+//  Copyright © 2018年 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
